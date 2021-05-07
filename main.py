28f7dfd7a56698af851d15d752bcf71bda2f9958
unknown <freerunner1317@yandex.ru>
2021-05-08 01:00:38 +0700

    g

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QRegExp
+from PyQt5.QtGui import QRegExpValidator
 
 
 def calculate():
     print("Рассчет")
+    window.lineEdit_ip.clear()
 
 def clear():
     print("clear")
+    window.lineEdit_ip.clear()
 
 class MainWindow(QMainWindow):
    def __init__(self):
@@ -25,7 +29,10 @@ if __name__ == '__main__':
     window.lineEdit_ip.editingFinished.connect(calculate)
     window.lineEdit_mask.editingFinished.connect(calculate)
 
-    window.lineEdit_ip.returnPressed.connect(clear)
+
+    reg_ex = QRegExp("([0-9]{1,3}\.){3}[0-9]{1,3}")
+    input_validator = QRegExpValidator(reg_ex, window.lineEdit_ip)
+    window.lineEdit_ip.setValidator(input_validator)
 
 
     window.show()
